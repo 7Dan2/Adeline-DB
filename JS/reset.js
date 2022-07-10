0f4068ba3df99d7850dcee02469143a248dbec88
Dan <37207556+7Dan2@users.noreply.github.com>
2022-07-10 21:19:59 +0200

    Add files via upload
    Add reset action

---
+++ JS/reset.js
@@ -0,0 +1,14 @@
+
+let getResetButton = document.querySelector("#btn_reset");
+
+getResetButton.addEventListener("click", clearAllDatas);
+
+function clearAllDatas()
+{
+    let getInput = document.querySelectorAll(".input");
+
+    for(i = 0 ; i < getInput.length ; i++)
+    {
+        getInput[i].value = "";
+    }
+};
