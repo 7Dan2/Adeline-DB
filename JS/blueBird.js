7f11a12921e058bd24a40486b73123a56b252669
Dan <37207556+7Dan2@users.noreply.github.com>
2022-07-11 15:24:27 +0200

    Add files via upload
    Version 1.0.0 is running

---
+++ JS/blueBird.js
@@ -0,0 +1,1 @@
+const getTitle = document.getElementById("app-title");
--- JS/mailto.js
+++ JS/mailto.js
@@ -14,16 +14,32 @@ function getDatas()
     // document.getElementById("C4_60_electronik").value
     // document.getElementById("C5_V_40_pneumatik").value
 
-    let datasArray =[{"C1_30_1_manual":C1_30_1_manual.value, 
-    "C1_30_1_pneumatik":C1_30_1_pneumatik.value,
-    "C1_30_2_manual":C1_30_2_manual.value,
-    "C1_30_2_pneumatik":C1_30_2_pneumatik.value,
-    "C3_20_manual":C3_20_manual.value,
-    "C3_20_electronik":C3_20_electronik.value,
-    "C4_60_pneumatik":C4_60_pneumatik.value,
-    "C4_60_electronik":C4_60_electronik.value,
-    "C5_V_40_pneumatik":C5_V_40_pneumatik.value
-    }];
+    
 
-    console.log (datasArray);
-}
+    let datasArray =["C1_30_1_manual", C1_30_1_manual.value, 
+    "C1_30_1_pneumatik",C1_30_1_pneumatik.value,
+    "C1_30_2_manual",C1_30_2_manual.value,
+    "C1_30_2_pneumatik",C1_30_2_pneumatik.value,
+    "C3_20_manual",C3_20_manual.value,
+    "C3_20_electronik",C3_20_electronik.value,
+    "C4_60_pneumatik",C4_60_pneumatik.value,
+    "C4_60_electronik",C4_60_electronik.value,
+    "C5_V_40_pneumatik",C5_V_40_pneumatik.value,
+    "user",C5_V_40_electronik.value
+    ];
+
+    
+    let uri = "mailto:unic.norton@gmail.com?&subject=";
+    let getDate = new Date();
+    let subject = "Releves" + " " + getDate.getDate() + getDate.getMonth() + getDate.getFullYear();
+    // let email = "unic.norton@gmail.com?";
+        // uri += encodeURIComponent(email);
+        uri += "&subject="
+        uri += encodeURIComponent(subject);
+        uri += "&body=";
+        uri += encodeURIComponent(datasArray);
+        window.open(uri);
+
+        // console.log (datasArray);
+        // console.log (uri);
+}              
